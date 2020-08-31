c0b180c9725cde1a717b2679482e598dc7c3c851
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-08-30 23:16:12 -0400

    add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../css/theme';
+import { GlobalStyles } from '../css/base';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
   );
