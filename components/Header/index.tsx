c27e4c4ee1e4b770d5ad027562bb2aa78cdb8e73
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-08-31 18:13:26 -0400

    page header

---
+++ components/Header/index.tsx
@@ -0,0 +1,77 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faUser,
+  faShoppingCart
+} from '@fortawesome/free-solid-svg-icons';
+
+const StyledNav = styled.nav`
+  width: 100%;
+  padding: 2rem 0;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  opacity: 0.7;
+
+  ul {
+    &:first-of-type {
+      margin: 0 15rem;
+    }
+
+    li {
+      display: inline-block;
+
+      &:not(:last-of-type) {
+        margin-right: 3.5rem;
+      }
+    }
+  }
+`;
+
+const StyledLink = styled.a`
+  font-size: 1.5rem;
+  font-weight: 200;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon)`
+  font-size: 1.5rem;
+  font-weight: 200;
+`;
+
+export default function Header() {
+  return (
+    <StyledNav>
+      <img src='/AmourEssentials-Logo.png' alt='Amour Essentials Logo' />
+      <ul>
+        <li>
+          <Link href='/'>
+            <StyledLink>Home</StyledLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='/shop'>
+            <StyledLink>Shop</StyledLink>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <StyledLink>About</StyledLink>
+          </Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <StyledIcon icon={faSearch} />
+        </li>
+        <li>
+          <StyledIcon icon={faUser} />
+        </li>
+        <li>
+          <StyledIcon icon={faShoppingCart} />
+        </li>
+      </ul>
+    </StyledNav>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import Head from 'next/head';
-import styled from 'styled-components';
 import { theme as t } from '../css/theme';
-
-const StyledHome = styled.div`
-  background-color: ${t.colors.primary};
-`;
+import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <StyledHome>
-      <h1>Home Page</h1>
-    </StyledHome>
+    <>
+      <Header />
+    </>
   );
 }
