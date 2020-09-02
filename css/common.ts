a101f652476b25ee7ffb2691f9e69a1432bd9985
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-09-01 23:05:43 -0400

    hover states

--- css/common.ts
+++ css/common.ts
@@ -16,18 +16,29 @@ export const ActionButton = styled.button<IProps>`
   font-size: 1.2rem;
   text-transform: uppercase;
   letter-spacing: 1px;
+  cursor: pointer;
   ${props => {
     switch (props.variant) {
       case VARIANT.SECONDARY:
         return `
           background-color: ${t.colors.primary};
           color: ${t.colors.white};
+
+          &:hover {
+            background-color: ${t.colors.grey};
+            color: ${t.colors.white};
+          }
         `;
       case VARIANT.PRIMARY:
       default:
         return `
           background-color: ${t.colors.white};
           color: ${t.colors.primary};
+
+          &:hover {
+            background-color: ${t.colors.lightGrey};
+            color: ${t.colors.white};
+          }
         `;
     }
   }}
