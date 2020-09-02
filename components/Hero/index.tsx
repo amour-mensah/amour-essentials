2d92972b4eac71acb0394ac74f88d94bfcfef56c
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-09-01 22:39:48 -0400

    hero section

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,15 +1,63 @@
 import styled from 'styled-components';
+import { theme as t } from '../../css/theme';
 
-const StyledHero = styled.div`
+const StyledHero = styled.section`
   background: url('/pexels-photo-54283.png');
+  background-repeat: no-repeat;
+  background-size: 100%;
   width: 100%;
   height: 100vh;
+`;
+
+const StyledHeroCTA = styled.div`
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  width: max-content;
+  color: ${t.colors.white};
+
+  h1 {
+    font-size: 3.5rem;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    font-size: 2.5rem;
+    font-family: ${t.fonts.secondary};
+    font-weight: 300;
+    margin-bottom: 4rem;
+  }
+`;
+
+const Italicized = styled.span`
+  font-style: italic;
+`;
+
+const StyledActionButton = styled.button`
+  border: none;
+  padding: 1rem 2.5rem;
+  background-color: ${t.colors.white};
+  font-size: 1.2rem;
+  color: ${t.colors.primary};
+  text-transform: uppercase;
+  letter-spacing: 1px;
 `;
 
 export default function Hero() {
   return (
     <StyledHero>
-      <h1>This is the Hero section</h1>
+      <StyledHeroCTA>
+        <h1>Header Title Gotham Book 70pt</h1>
+        <p>
+          Description Text Garamond Premier Pro Regular or{' '}
+          <Italicized>Italic</Italicized> 45pt
+        </p>
+        <StyledActionButton>Action Button</StyledActionButton>
+      </StyledHeroCTA>
     </StyledHero>
   );
 }
