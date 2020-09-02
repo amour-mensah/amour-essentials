a82dab61c2e730e6ca37cad24fc5480ff683c662
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-09-01 21:44:31 -0400

    add secondary font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           href='https://fonts.cdnfonts.com/css/gotham-book'
           rel='stylesheet'
         />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,700;1,400;1,700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
