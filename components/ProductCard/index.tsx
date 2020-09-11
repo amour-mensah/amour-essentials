e410a0281835da2ddf788b4608dc564d2a6747fe
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-09-10 21:02:22 -0400

    product card image width

--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -5,11 +5,6 @@ const StyledProductCard = styled.div`
   padding: 1rem;
   color: ${t.colors.white};
   text-align: center;
-  /* margin: 0 1.5rem 2rem 1.5rem; */
-
-  /* @media (max-width: 425px) {
-    margin: 0 auto 2rem auto;
-  } */
 
   p {
     margin-bottom: 1rem;
@@ -17,8 +12,8 @@ const StyledProductCard = styled.div`
 `;
 
 const StyledProductImage = styled.div`
-  width: 12rem;
-  height: 18rem;
+  width: 18rem;
+  height: 22rem;
   margin: 0 auto 2rem auto;
   background-color: ${t.colors.white};
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
