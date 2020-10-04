822bc7b8eb61f1ca8850eb88286d16552407458b
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-10-04 17:57:31 -0400

    refactor styles for reusability

---
+++ components/SurveyForm/steps/ShareFeedback.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyledShareFeedback } from './Steps.styled';
+
+export default function ShareFeedback() {
+  return (
+    <StyledShareFeedback>
+      <h3>Thanks for your Feedback!</h3>
+      <p>IN THE MEANTIME, WOULD YOU LIKE TO SHARE YOUR FEEDBACK TO OTHERS?</p>
+      <p>
+        ALTHOUGH SHARING YOUR FEEDBACK IS NOT REQUIRED, CUSTOMERS TRULY VALUE
+        FEEDBACK WRITTEN BY OTHER CUSTOMERS WHO PURCHASED THE PRODUCT.
+      </p>
+      <p>
+        FURTHERMORE, FEEDBACK LIKE YOURS HELPS SMALL BRANDS CONTINUE TO GROW
+        ONLINE AND IS VERY MUCH APPRECIATED.
+      </p>
+    </StyledShareFeedback>
+  );
+}
