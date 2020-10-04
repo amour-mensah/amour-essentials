dd2be1db8e61272daa0ba352ac5354f7d1d0352d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-10-04 16:53:18 -0400

    create experience step and fix layout issues

--- components/SurveyForm/steps/OrderId.tsx
+++ components/SurveyForm/steps/OrderId.tsx
@@ -1,53 +1,30 @@
 import React from 'react';
-import styled from 'styled-components';
-import { theme as t } from '../../../css/theme';
 import { ActionButton } from '../../../css';
+import { StyledOrderId } from './Steps.styled';
 
 interface OrderIdProps {
   orderId: String;
   setOrderId: any;
+  nextStep: any;
 }
 
-const StyledOrderId = styled.div`
-  width: 65%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-  border-radius: 1rem;
-  padding: 2.5rem;
+export default function OrderId({
+  orderId,
+  setOrderId,
+  nextStep
+}: OrderIdProps) {
+  // Authenticate order ID
+  const AuthenticateOrderId = e => {
+    e.preventDefault();
+    // Validate orderId
+    // Handle real authentication in here
+    nextStep();
+  };
 
-  h2 {
-    font-size: 2rem;
-    text-transform: uppercase;
-    margin-bottom: 6rem;
-  }
-
-  input {
-    background-color: transparent;
-    text-indent: 1.5rem;
-    padding: 0.8rem 1.5rem;
-    margin-right: 1rem;
-    border: none;
-    border-bottom: 1px solid #000000;
-    width: 40rem;
-    font-size: 1.2rem;
-    transition: border ease-in-out 0.2s;
-
-    &:focus {
-      outline: none;
-      border-bottom: 3px solid ${t.colors.primary};
-    }
-  }
-`;
-
-export default function OrderId({ orderId, setOrderId }: OrderIdProps) {
   return (
     <StyledOrderId>
       <h2>Enter Order ID Below</h2>
-      <form>
+      <form onSubmit={AuthenticateOrderId}>
         <input
           type='text'
           name='orderId'
