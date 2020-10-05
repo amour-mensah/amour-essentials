73f70032c123522c1c0387a77f8304c9078116bf
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-10-04 23:22:31 -0400

    success step

--- components/SurveyForm/steps/Success.tsx
+++ components/SurveyForm/steps/Success.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { StyledSuccess } from './Steps.styled';
 
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <StyledSuccess>
+      <h3>Thank You for Claiming Your Promotional Discount</h3>
+      <p>
+        THANK YOU SO MUCH FOR YOUR FEEDBACK! YOUR PROMOTIONAL DISCOUNT CODE WILL
+        BE EMAILED TO YOU SHORTLY! KEEP AN EYE ON YOUR INBOX - WE’LL BE SENDING
+        YOU FUTURE INFORMATION ON PRODUCT RELEASES. PLEASE ALLOW 5-7 BUSINESS
+        DAYS TO RECEIVE YOUR DISCOUNT CODE.
+      </p>
+      <p>
+        IF WE SHOULD HAVE ANY ADDITIONAL QUESTIONS ABOUT YOUR REGISTRATION
+        INFORMATION, OUR PROMOTIONS TEAM WILL CONTACT YOU VIA EMAIL WITH
+        ASSISTANCE TO COMPLETE THE REQUIRED STEPS.
+      </p>
+    </StyledSuccess>
+  );
 }
